017eee296b040641fcdcdabc1435b1d44fd1664d
Samuel Lao <samuel.lao@ivirma.com>
2020-05-02 17:24:05 +0200

    SL. Version 2

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var app = express();
 var router = express.Router();
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!\n');
+  response = 'This is version 2 of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
 });
 
 app.post('/', function(req, res) {
